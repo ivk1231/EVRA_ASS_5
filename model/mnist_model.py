0df26f6da918114e276e3348f42bc863cadf8f64
Ayush Verma <immanuelvkoshy@gmail.com>
2024-11-21 23:49:48 +0530

    2

--- model/mnist_model.py
+++ model/mnist_model.py
@@ -5,21 +5,18 @@ import torch.nn.functional as F
 class CompactMNIST(nn.Module):
     def __init__(self):
         super(CompactMNIST, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 3 * 3, 10)
         
     def forward(self, x):
-        x = self.bn1(F.relu(self.conv1(x)))
+        x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
-        
-        x = self.bn2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-        
-        x = x.view(-1, 32 * 7 * 7)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 32 * 3 * 3)
+        x = self.fc1(x)
         return F.log_softmax(x, dim=1) 
